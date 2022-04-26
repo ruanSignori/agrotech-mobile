888a20d2bc0ff12fdd0c9de436acb20018ba0d42
ruanSignori <cleosignori@gmail.com>
2022-04-26 07:08:26 -0300

    configure routes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
-import { Home } from "./src/screens/home";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { StatusBar } from "react-native";
+
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <StatusBar translucent />
+      <AppRoutes />
+    </NavigationContainer>
+  );
 }
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,45 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import { LogBox, Text, ActivityIndicator } from "react-native";
+import React from "react";
+import { LogBox } from "react-native";
 
-import { database, ref, onValue } from "../../services/firebase";
+import { DataGarden } from "../../components/dataGarden";
+import { Header } from "../../components/header";
 import * as S from "./style";
 
 LogBox.ignoreLogs(["Setting a timer"]);
 
-type dataOfGarden = {
-  temperature: number;
-  humidity: number;
-};
-
 export function Home(): JSX.Element {
-  const [dataGarden, setDataGarden] = useState<dataOfGarden>({
-    humidity: 0,
-    temperature: 0,
-  });
-
-  useEffect(() => {
-    const statsGardenRef = ref(database, "/stats_garden");
-
-    onValue(statsGardenRef, (value) => {
-      const data: dataOfGarden = value.val();
-      setDataGarden({ ...data });
-    });
-  }, []);
-
   return (
-    <>
-      <StatusBar translucent />
-      <S.Container>
-        {!dataGarden.humidity && !dataGarden.temperature ? (
-          <ActivityIndicator color="#34D399" />
-        ) : (
-          <>
-            <Text>Umidade do solo {dataGarden.humidity}%</Text>
-            <Text>Temperatura do ambiente {dataGarden.temperature} C°</Text>
-          </>
-        )}
-      </S.Container>
-    </>
+    <S.Container>
+      <Header />
+      <DataGarden />
+    </S.Container>
   );
 }
