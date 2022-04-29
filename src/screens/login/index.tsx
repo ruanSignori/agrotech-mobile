361248f5375ef28a83f62e771ffacd5a6cee925a
ruanSignori <cleosignori@gmail.com>
2022-04-29 08:09:24 -0300

    template page login

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+import * as S from "./style";
+
+export function Login() {
+  return (
+    <S.Container>
+      <S.Container />
+      <Text>LOGIN</Text>
+    </S.Container>
+  );
+}
